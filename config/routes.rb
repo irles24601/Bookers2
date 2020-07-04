da8a443d5bdae21d91f9cf4acc630cd47b688412
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:59:21 +0000

    [Add] Add Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  get 'users/show'
+  root 'home#index'
+  get "home/about" => "home#about"
   devise_for :users
   resources :books, only: [:new, :create, :index, :show]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
